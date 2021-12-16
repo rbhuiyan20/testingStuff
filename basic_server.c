bf8aeb13774df13c978b6d76151db5223f8e8d01
rbhuiyan20 <90663629+rbhuiyan20@users.noreply.github.com>
2021-12-16 01:03:29 -0500

    Update basic_server.c

--- basic_server.c
+++ basic_server.c
@@ -3,8 +3,25 @@
 
 int main() {
 
-  int to_client;
-  int from_client;
+    int to_client;
+    int from_client;
 
-  from_client = server_handshake( &to_client );
+    while (1) {
+        // Repeatedly starts handshake
+        from_client = server_handshake( &to_client );
+
+        char line[BUFFER_SIZE];
+        while (1) { // Prompt client for input
+            if (read(from_client, line, sizeof(line))==0) { // reads data from client
+                break;
+            }
+
+            // process string to all lower
+            for(int i=0; i<strlen(line); i++) {
+                line[i] = tolower(line[i]);
+            }
+
+            write(to_client, line, sizeof(line)); // send data to client
+        }
+    }
 }
