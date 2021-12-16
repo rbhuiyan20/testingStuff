d89abd35bfdaf815b9110c2d509d56c8c4dfac76
rbhuiyan20 <90663629+rbhuiyan20@users.noreply.github.com>
2021-12-16 01:03:04 -0500

    Update basic_client.c

--- basic_client.c
+++ basic_client.c
@@ -3,8 +3,20 @@
 
 int main() {
 
-  int to_server;
-  int from_server;
+    int to_server;
+    int from_server;
 
-  from_server = client_handshake( &to_server );
+    // Starts the handshake
+    from_server = client_handshake( &to_server );
+
+    char line[BUFFER_SIZE];
+    while (1) {
+        // Prompts user for input
+        printf("Relay Message to Server: ");
+        fgets(line, BUFFER_SIZE, stdin);
+        
+        write(to_server, line, sizeof(line)); // send data to server
+        read(from_server, line, sizeof(line)); // reads processed data from server
+        printf("Response From Server: %s\n", line); // displays processed data to server
+    }
 }
