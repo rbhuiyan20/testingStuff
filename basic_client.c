1b8e3fbbd480a731307dfaed7a220f3d4e594a93
rbhuiyan20 <90663629+rbhuiyan20@users.noreply.github.com>
2022-01-09 23:13:58 -0500

    Add files via upload

--- basic_client.c
+++ basic_client.c
@@ -2,9 +2,17 @@
 
 
 int main() {
-
-  int to_server;
-  int from_server;
-
-  from_server = client_handshake( &to_server );
+    int sd = client_connect();
+    char line[BUFFER_SIZE];
+    while(1) {
+        // while client runs, prompt user for input
+        printf("input: ");
+        fgets(line, sizeof(line), stdin);
+        // send input to server
+        write(sd, line, sizeof(line));
+        // get the processed output from the server
+        read(sd, line, sizeof(line));
+        // print it for the user
+        printf("Output: %s", line);
+    }
 }
--- basic_server.c
+++ basic_server.c
@@ -3,8 +3,44 @@
 
 int main() {
 
-  int to_client;
-  int from_client;
+    // listening for connection
+    int sd = server_setup();
 
-  from_client = server_handshake( &to_client );
+    while (1) {
+        // accept client connection
+        int from_client = server_connect(sd);
+        if(from_client == -1) {
+            printf("unable to accept client connection");
+            return 0;
+        }
+
+        int f = fork();
+        if (f) { // server
+
+        } else { // subserver
+            char line[BUFFER_SIZE];
+
+            while (1) { // Prompt client for input
+                if (read(from_client, line, sizeof(line))==0) { // reads data from client
+                    int err = close(from_client);
+                    if(err == -1) {
+                        printf("Unable to read from client");
+                        return 0;
+                    }
+                    break;
+                }
+
+                // process string to all lower
+                for(int i=0; i<strlen(line); i++) {
+                    line[i] = tolower(line[i]);
+                }
+
+                int err = write(from_client, line, sizeof(line)); // send data to client
+                if(err == -1) {
+                    printf("Unable to write to client");
+                    return 0;
+                }
+            }
+        }
+    }
 }
