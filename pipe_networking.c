56a72f79fa596a834bbac825966dcc3fcb358ba4
rbhuiyan20 <90663629+rbhuiyan20@users.noreply.github.com>
2022-01-09 23:49:43 -0500

    Add files via upload

--- pipe_networking.c
+++ pipe_networking.c
@@ -1,25 +1,23 @@
 #include "pipe_networking.h"
 
 int server_setup() {
+    printf("creating socket\n");
     struct addrinfo * hints, * results;
     hints = calloc(1,sizeof(struct addrinfo));
-
     hints->ai_family = AF_INET;
-    hints->ai_socktype = SOCK_STREAM; 
-    hints->ai_flags = AI_PASSIVE; 
-    getaddrinfo(NULL, "9845", hints, &results);  
-  
-   
-    int sd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
-   
+    hints->ai_socktype = SOCK_STREAM; //TCP socket
+    hints->ai_flags = AI_PASSIVE; //only needed on server
+    getaddrinfo(NULL, "9001", hints, &results);  //Server sets node to NULL
 
+    printf("binding socket\n");
+    //create socket
+    int sd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
     bind(sd, results->ai_addr, results->ai_addrlen);
-  
 
     printf("listening on socket\n");
     listen(sd, 10);
-    
 
+    printf("freeing stuff\n");
     free(hints);
     freeaddrinfo(results);
 
@@ -41,18 +39,16 @@ int client_connect() {
     struct addrinfo * hints, * results;
     hints = calloc(1,sizeof(struct addrinfo));
     hints->ai_family = AF_INET;
-    hints->ai_socktype = SOCK_STREAM; 
-    hints->ai_flags = AI_PASSIVE; 
-    getaddrinfo("127.0.0.1", "9845", hints, &results);  
-    
+    hints->ai_socktype = SOCK_STREAM; //TCP socket
+    hints->ai_flags = AI_PASSIVE; //only needed on server
+    getaddrinfo("127.0.0.1", "9001", hints, &results);  //Server sets node to NULL
 
-  
+    //create socket
     int sd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
-    
 
     connect(sd, results->ai_addr, results->ai_addrlen);;
-    
-    
+
+    //DO STUFF
 
     free(hints);
     freeaddrinfo(results);
